f13aa69378c132794001dc52fe7a466199847fda
hadl1342 <charlotte@uidaho.edu>
2018-03-29 14:45:20 -0700

    initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+node.default['haproxy']['members'] = [{
+   "hostname" => "ec2-107-21-196-89.compute-1.amazonaws.com",
+   "ipaddress" => "107.21.196.89",
+   "port" => 80,
+   "ssl_port" => 80
+ }]
+
+include_recipe 'haproxy::manual'
